63cda171cee5e77e48df64d7b06aeed62a6b8582
Paulo Sérgio <ps131296@gmail.com>
2021-03-17 21:39:24 -0300

    make sure FirebaseExpensesLoader requests URL on load function call

--- MyFinancesTests/FirebaseExpensesLoaderTests.swift
+++ MyFinancesTests/FirebaseExpensesLoaderTests.swift
@@ -8,21 +8,54 @@
 import XCTest
 @testable import MyFinances
 
-class FirebaseExpensesLoader {
+protocol HTTPClient {
+    func get(url: URL)
+}
 
+class FirebaseExpensesLoader {
+    let url: URL
+    let client: HTTPClient
+
+    init(url: URL,  client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    func load() {
+        client.get(url: url)
+    }
 }
 
 class FirebaseExpensesLoaderTests: XCTestCase {
 
     func test_init_doesMakeRequests() {
+        let url = URL(string: "http://any-url.com")!
         let client = HTTPClientSpy()
-        _ = FirebaseExpensesLoader()
+        _ = FirebaseExpensesLoader(url: url, client: client)
 
         XCTAssertEqual(client.requestCount, 0)
     }
 
-    class HTTPClientSpy {
+    func test_load_makeRequestWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let client = HTTPClientSpy()
+        let sut = FirebaseExpensesLoader(url: url, client: client)
+
+        sut.load()
+
+        XCTAssertEqual(client.requestCount, 1)
+        XCTAssertEqual(client.requestedUrl, url)
+
+    }
+
+    class HTTPClientSpy: HTTPClient {
         var requestCount: Int = 0
+        var requestedUrl: URL? = nil
+
+        func get(url: URL) {
+            requestCount += 1
+            requestedUrl = url
+        }
     }
 
 }
