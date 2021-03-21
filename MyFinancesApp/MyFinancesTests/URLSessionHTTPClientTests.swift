6873eeb52c9a99dc919430409ae98110d32d5b5a
Paulo Sérgio <ps131296@gmail.com>
2021-03-21 09:15:19 -0300

    ensure URLSessionHTTPClient makes request on "get"

--- MyFinancesApp/MyFinancesTests/URLSessionHTTPClientTests.swift
+++ MyFinancesApp/MyFinancesTests/URLSessionHTTPClientTests.swift
@@ -5,4 +5,40 @@
 //  Created by Paulo Sergio da Silva Rodrigues on 19/03/21.
 //
 
-import Foundation
+import XCTest
+import MyFinances
+
+class URLSessionHTTPClient {
+    let session: URLSession
+
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    func get(_ url: URL, completion: @escaping () -> Void) {
+        session.dataTask(with: url)
+    }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+
+        sut.get(url) { }
+
+        XCTAssertEqual(session.requestedURLs, [url])
+    }
+
+    class URLSessionSpy: URLSession {
+        var requestedURLs = [URL]()
+
+        override func dataTask(with url: URL) -> URLSessionDataTask {
+            requestedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+
+        class FakeURLSessionDataTask: URLSessionDataTask {}
+    }
+}
