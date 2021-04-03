248719a5ca88219b40cd9d2e9f8faf6b6da8538b
Paulo Sérgio <ps131296@gmail.com>
2021-04-03 11:35:32 -0300

    add iOS target

---
+++ MyFinancesApp/MyFinancesiOS/MyFinancesiOS.h
@@ -0,0 +1,18 @@
+//
+//  MyFinancesiOS.h
+//  MyFinancesiOS
+//
+//  Created by Paulo Sergio da Silva Rodrigues on 03/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFinancesiOS.
+FOUNDATION_EXPORT double MyFinancesiOSVersionNumber;
+
+//! Project version string for MyFinancesiOS.
+FOUNDATION_EXPORT const unsigned char MyFinancesiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFinancesiOS/PublicHeader.h>
+
+
