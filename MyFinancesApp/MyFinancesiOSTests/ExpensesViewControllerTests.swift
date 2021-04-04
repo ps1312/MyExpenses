73c38e29b828e0bf377163442b75d650352ba959
Paulo Sérgio <ps131296@gmail.com>
2021-04-04 11:34:32 -0300

    extracted ExpensesViewController SUT creation to a helper method, added memory leak tracking

--- MyFinancesApp/MyFinancesiOSTests/ExpensesViewControllerTests.swift
+++ MyFinancesApp/MyFinancesiOSTests/ExpensesViewControllerTests.swift
@@ -30,15 +30,13 @@ class ExpensesViewController: UITableViewController {
 
 class ExpensesViewControllerTests: XCTestCase {
     func test_init_doesNotLoadExpenses() {
-        let loaderSpy = LoaderSpy()
-        _ = ExpensesViewController(loader: loaderSpy)
+        let (_, loaderSpy) = makeSUT()
 
         XCTAssertEqual(loaderSpy.callsCount, 0)
     }
 
     func test_viewDidLoad_loadExpenses() {
-        let loaderSpy = LoaderSpy()
-        let sut = ExpensesViewController(loader: loaderSpy)
+        let (sut, loaderSpy) = makeSUT()
 
         sut.loadViewIfNeeded()
 
@@ -46,18 +44,23 @@ class ExpensesViewControllerTests: XCTestCase {
     }
 
     func test_pullToRefresh_loadExpenses() {
-        let loaderSpy = LoaderSpy()
-        let sut = ExpensesViewController(loader: loaderSpy)
+        let (sut, loaderSpy) = makeSUT()
 
         sut.loadViewIfNeeded()
 
         sut.refreshControl?.simulatePullToRefresh()
-
         XCTAssertEqual(loaderSpy.callsCount, 2)
 
         sut.refreshControl?.simulatePullToRefresh()
-
         XCTAssertEqual(loaderSpy.callsCount, 3)
+    }
+
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> (ExpensesViewController, LoaderSpy) {
+        let loaderSpy = LoaderSpy()
+        let sut = ExpensesViewController(loader: loaderSpy)
+        testMemoryLeak(loaderSpy, file: file, line: line)
+        testMemoryLeak(sut, file: file, line: line)
+        return (sut,  loaderSpy)
     }
 
     class LoaderSpy: ExpensesLoader {
