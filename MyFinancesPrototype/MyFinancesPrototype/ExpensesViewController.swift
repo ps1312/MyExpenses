b3e432fc7d9fc683c0a98957bcde9fcbd759de77
Paulo Sérgio <ps131296@gmail.com>
2021-04-03 21:35:36 -0300

    add loading preview to prototype

--- MyFinancesPrototype/MyFinancesPrototype/ExpensesViewController.swift
+++ MyFinancesPrototype/MyFinancesPrototype/ExpensesViewController.swift
@@ -8,8 +8,28 @@
 import UIKit
 
 class ExpensesViewController: UITableViewController {
+    var count: Int = 0
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        tableView.tableFooterView = UIView()
+
+        refresh()
+    }
+
+    @IBAction func refresh() {
+        refreshControl?.beginRefreshing()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) { [weak self] in
+            self?.refreshControl?.endRefreshing()
+            self?.count = 50
+            self?.tableView.reloadData()
+        }
+    }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
