926395c4e51a51669936633c0586c588b94a95f0
Paulo Sérgio <ps131296@gmail.com>
2021-04-10 21:44:19 -0300

    removed unused method

--- MyFinancesApp/MyFinancesiOS/Expenses UI/Composers/ExpensesUIComposer.swift
+++ MyFinancesApp/MyFinancesiOS/Expenses UI/Composers/ExpensesUIComposer.swift
@@ -21,15 +21,6 @@ public final class ExpensesUIComposer {
 
         return expensesController
     }
-
-    private static func adaptExpensesModelsToCellControllers(expensesController: ExpensesViewController) -> (([ExpenseItem]) -> Void) {
-        return { [weak expensesController] items in
-            expensesController?.cellControllers = items.map { model in
-                let viewModel = ExpenseCellViewModel(model: model)
-                return ExpenseCellViewController(viewModel: viewModel)
-            }
-        }
-    }
 }
 
 class WeakRefVirtualProxy<T: AnyObject>: ExpensesLoadView where T: ExpensesLoadView {
